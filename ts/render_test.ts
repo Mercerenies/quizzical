035a01d099f08aa646091421d23813a9c5c68f75
Silvio Mayolo <mercerenies@comcast.net>
2021-09-29 22:06:28 -0400

    inline latex

--- ts/render_test.ts
+++ ts/render_test.ts
@@ -32,11 +32,48 @@ async function doMarkdown() {
 
 // TODO Integration with highlight.js?
 function setupMarkedOptions() {
+
   marked.setOptions({
     smartLists: true,
     xhtml: true,
   });
+
+  marked.use({ extensions: [InlineLatex] });
+
 }
+
+const InlineLatex = {
+  name: "inlineLatex",
+  level: "inline",
+
+  start(src: string): number | undefined {
+    const match = src.match(/\$/);
+    if (match) {
+      return match.index;
+    } else {
+      return undefined;
+    }
+  },
+
+  tokenizer(src: string) {
+    const match = src.match(/^\$((?:[^$\n]|\\[^\n])+)\$/);
+    if (match) {
+      return {
+        type: "inlineLatex",
+        raw: match[0],
+        inlineLatex: match[1],
+      };
+    } else {
+      return undefined;
+    }
+  },
+
+  renderer(token: any) {
+    const mml = MathJax.tex2mml(token.inlineLatex, { display: false });
+    return mml;
+  }
+
+};
 
 export async function renderMarkdown(text: string): Promise<string> {
   const rendered = marked(text);
