9331d2a1eaae29dba4892da75d1dfc2c382e8b58
Silvio Mayolo <mercerenies@comcast.net>
2021-10-01 21:24:30 -0400

    GameInitializer class

--- ts/game_initializer.ts
+++ ts/game_initializer.ts
@@ -1,4 +1,9 @@
 
+import { HostLobby } from './lobby.js';
+import { PlayerListUpdater } from './game_initializer/player_list_updater.js';
+import { ActiveScreen } from './active_screen.js';
+import { updateHeader } from './game_play_page.js';
+
 export const DEFAULT_MAX_PLAYERS = 4;
 
 /**
@@ -6,6 +11,50 @@ export const DEFAULT_MAX_PLAYERS = 4;
  * from the host's perspective.
  */
 export class GameInitializer {
+  readonly lobby: HostLobby;
+  private updater: PlayerListUpdater;
+  private activeScreen: ActiveScreen;
+
+  constructor(params: GameInitializerParams) {
+    this.lobby = params.lobby;
+    this.updater = new PlayerListUpdater(this.lobby, params.playerListDOM);
+    this.activeScreen = new ActiveScreen(this.lobby);
+
+    this.lobby.addListener(this.updater);
+    this.updater.update();
+
+  }
+
+  /**
+   * @return whether or not the parameters are right to start the game
+   */
+  canStartGame(): boolean {
+    return (this.lobby.playerCount > 0);
+  }
+
+  /**
+   * Starts the game proper.
+   */
+  async startGame(): Promise<void> {
+    if (!this.canStartGame()) {
+      return;
+    }
+
+    this.lobby.startGame();
+    console.log("Starting game...");
+
+    const newPage = $(await $.get('/game/play'));
+    $("main").replaceWith(newPage);
+    updateHeader(this.lobby, $("main"));
+
+  }
 
 }
 
+/**
+ * Type of argument to GameInitializer constructor.
+ */
+export interface GameInitializerParams {
+  lobby: HostLobby;
+  playerListDOM: JQuery<HTMLElement>;
+}
--- ts/main.ts
+++ ts/main.ts
@@ -5,24 +5,10 @@
  * @module main
  */
 
-import { hostLobby, HostLobby } from './lobby.js';
+import { hostLobby } from './lobby.js';
 import { DebugLobbyListener } from './lobby/debug_listener.js';
-import { updateHeader } from './game_play_page.js';
-import { ActiveScreen } from './active_screen.js';
 import { DEFAULT_MAX_PLAYERS } from './game_initializer.js';
-import { PlayerListUpdater } from './game_initializer/player_list_updater.js';
-
-async function startGame(lobby: HostLobby): Promise<void> {
-  if (lobby.playerCount > 0) {
-    lobby.startGame();
-    console.log("Starting game...");
-
-    const newPage = $(await $.get('/game/play'));
-    $("main").replaceWith(newPage);
-    updateHeader(lobby, $("main"));
-
-  }
-}
+import { GameInitializer } from './game_initializer.js';
 
 /**
  * Set up the game page. Should be called once after the page is
@@ -31,17 +17,13 @@ async function startGame(lobby: HostLobby): Promise<void> {
 export function setupNewGame(): void {
 
   hostLobby(DEFAULT_MAX_PLAYERS).then((lobby) => {
-    const updater = new PlayerListUpdater(lobby, $("#player-list"));
-    const _activeScreen = new ActiveScreen(lobby);
-
-    lobby.addListener(new DebugLobbyListener());
-    lobby.addListener(updater);
-
-    updater.update();
+    const gameInitializer = new GameInitializer({
+      lobby: lobby,
+      playerListDOM: $("#player-list"),
+    });
+    gameInitializer.lobby.addListener(new DebugLobbyListener());
     $("#code").text(lobby.code);
-
-    $("#start-game").click(() => startGame(lobby));
-
+    $("#start-game").click(() => gameInitializer.startGame());
   });
 
 }
