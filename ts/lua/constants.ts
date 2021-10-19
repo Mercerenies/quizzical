9a6cd91fe0ce0be8d4a6716a0d3b6ba321390b02
Silvio Mayolo <mercerenies@comcast.net>
2021-10-18 21:11:27 -0400

    Some helper constants

--- ts/lua/constants.ts
+++ ts/lua/constants.ts
@@ -3,6 +3,8 @@ export type pointer = number;
 
 export const NULL = 0;
 export const LUA_MULTRET = -1;
+export const LUAI_MAXSTACK = 1000000; // This is bad if we're somehow running on a 16-bit architecture
+export const LUA_REGISTRYINDEX = -LUAI_MAXSTACK - 1000;
 
 // These constants are borrowed from lua.h.
 
